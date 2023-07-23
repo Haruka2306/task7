93c04b7981c4820539702ee4aa2c38038f5e1fe3
Haruka Tsuchiya <solap5hyk237@gmail.com>
2023-07-23 09:00:06 +0900

    プロジェクトをセットアップ

--- src/main/java/com/example/task7/Profile.java
+++ src/main/java/com/example/task7/Profile.java
@@ -1,11 +1,26 @@
 package com.example.task7;
 
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
+
 public class Profile {
+    //入力必須、長さ20桁まで
+    @NotEmpty(message = "Please provide a name")
+    @Length(max = 20)
     private String name;
-    private String dateOfBirth;
+
+    //入力必須
+    @NotEmpty(message = "Please provide a dateOfBirth")
+    private  LocalDate dateOfBirth;
+
+    //対象年齢：20~65歳
+    @Min(20)
+    @Max(65)
     private int age;
 
-    public Profile(String name, String dateOfBirth, int age) {
+    public Profile(String name, LocalDate dateOfBirth, int age) {
         this.name = name;
         this.dateOfBirth = dateOfBirth;
         this.age = age;
@@ -14,12 +29,15 @@ public class Profile {
     public String getName() {
         return name;
     }
-
-    public String getDateOfBirth() {
+    public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
-
     public int getAge() {
         return age;
     }
 }
+
+
+
+
+
--- src/main/java/com/example/task7/ProfileController.java
+++ src/main/java/com/example/task7/ProfileController.java
@@ -1,19 +1,29 @@
 package com.example.task7;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+
 public class ProfileController {
     @GetMapping("/profiles")
-    public List<Profile> getProfiles(){
-        return List.of(new Profile("tanaka", "1989/01/01", 34),
-                       new Profile("yamada", "1991/03/01", 32));
+    public List<Profile> processData(
+            @Validated
+            @RequestParam String name,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dateOfBirth,
+            @RequestParam int age) {
+
+        return List.of(new Profile("tanaka", (LocalDate.of(1989,01,01)),34),
+                       new Profile("yamada", (LocalDate.of(1979,01,01)),44));
     }
+
 
     @PostMapping("/profiles")
     public ResponseEntity<ProfileCreateResponse> create(@RequestBody ProfileCreateForm form){
